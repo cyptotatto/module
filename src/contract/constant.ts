5bc9ea801a188ef40f3d7918d102fd30c821863e
INBUM02 <kib0202@naver.com>
2022-10-23 15:26:26 +0900

    FEAT:mint module

--- src/contract/constant.ts
+++ src/contract/constant.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 
-export const TattoRoleAddress = "0x94476c2E4813490404D79056B8127104e2Fa00ad";
-export const TattoCurrencyAddess = "0xA9fe913C2B58Ecb979382212c2DaB05012163153";
-export const TattoMarketAddress = "0x63A339D0C4a1fE38abBB182F778d04CD30087330";
+export const TattoRoleAddress = "0x87974cb4E05f102A9527F2f185Cc8641D548bEA5";
+export const TattoCurrencyAddess = "0xd701c4C2d29660b8Dd91ca1254A988Ac1316F2c3";
+export const TattoMarketAddress = "0x0DcC01a5d496A6c5F58b3CDe335cE861795913C3";
 export const TattoCollectionAddress =
-  "0x89010AEceb9c27e735EF288D91B43Aa5e448acAc";
+  "0x48501804b332d642c4b14580574e26A949A3D2c1";
